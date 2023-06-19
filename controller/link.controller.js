ade18a9a2fd7a002fd0ac5a620bdb6de5e131b02
Nao MAUSSERVEY <nao.mausservey@efrei.net>
2023-06-19 09:59:31 +0200

    trying to fix

--- controller/link.controller.js
+++ controller/link.controller.js
@@ -1,6 +1,5 @@
 const { ObjectId } = require("mongodb");
 const LinkModel = require("../models/links.model");
-const StatsModel = require("../models/stats.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 const randomstring = require("randomstring");
 
